a8f6eddc88e5fc2231f3711a94b989bb1717f9fb
alexandrejablon <alexandre@alexandre-SATELLITE-U500.(none)>
2013-06-02 17:14:32 +0200

    Added license terms in the files.

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-//
-// app.js
-//
-// Defines the application parameters and paths for queries
-//
+/*Copyright (c) 2013, Alexandre Jablon
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * app.js
+ * 
+ * Defines the application parameters and paths for queries
+ */
 
 // Using Express, a web application framework and module for node.js
 // This module is not a built-in module (see package.json for more
@@ -67,6 +93,11 @@ app.get ('/api/find_all', api.rest_find_all);
 // Landing error code for method not allowed
 app.all ('/api/find_all', function(request, response) {
     _send_error (request, response, 405);
+});
+
+// Static 404 error
+app.all ('/static(/*)?', function(request, response) {
+    response.send ("<html><head><title>404 - Not found</title></head><body><h1>404 - Not found</h1></body></html>", 404);
 });
 
 // Default landing error code
--- nodegraph-api.js
+++ nodegraph-api.js
@@ -1,8 +1,38 @@
-//
-// nodegraph-api.js
-//
-// Defines the methods for the API itself
-//
+/*Copyright (c) 2013, Alexandre Jablon
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * nodegraph-api.js
+ * 
+ * Defines the methods for the API itself
+ */
+
+// Default mongoDB hosts and port number
+var mongo_host = 'localhost';
+var mongo_port = 27017;
 
 // The app uses a couple modules and drivers
 //
@@ -196,8 +226,7 @@ function _get_xml (url_string, callback) {
             callback (null, chunk);
         });
     }).on('error', function(error) {
-        console.error (error);
-        callback (error, null);
+        callback (error.message, null);
     });
 }
 
@@ -293,7 +322,7 @@ function _insert_document (json_object, callback) {
 // for Node.js
 function _db_start (callback) {
     if (server == undefined) {
-        server = new mongo.Server('localhost', 27017, {auto_reconnect: true});
+        server = new mongo.Server(mongo_host,  mongo_port, {auto_reconnect: true});
         db = new mongo.Db('nodegraphDB', server, {safe: true});
         db.open(function(error, db) {
             if(!error) {
